cad8d4644c4726f51433f2807904525019ea661a
Petlevannyy-Sergey <spetlevannyy@mail.ru>
2025-10-18 23:16:52 +0300

    Тесты для проверки удаления курьера

---
+++ src/test/java/courier/DeleteCourierTests.java
@@ -0,0 +1,64 @@
+package courier;
+
+import io.qameta.allure.junit4.DisplayName;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import login.Login;
+import org.apache.http.HttpStatus;
+import org.junit.Before;
+import org.junit.Test;
+import utils.RandomUtils;
+
+import static org.hamcrest.Matchers.equalTo;
+
+public class DeleteCourierTests {
+    @Before
+    public void setUp() {
+        RestAssured.baseURI = "https://qa-scooter.praktikum-services.ru/";
+    }
+
+    @Test
+    @DisplayName("Удаление курьера с валидным id")
+    public void deleteCourierWithCorrectIdIsSuccess() {
+        // Arrange
+        Courier courier = new Courier(RandomUtils.GenerateLogin(), RandomUtils.GeneratePassword(), RandomUtils.GenerateFirstName());
+
+        // Act
+        CourierActions.create(courier);
+        String id = CourierActions.login(new Login(courier.getLogin(), courier.getPassword())).then().extract().path("id").toString();
+        Response response = CourierActions.delete(id);
+
+        // Assert
+        response.then().assertThat().statusCode(HttpStatus.SC_OK)
+                .and()
+                .body("ok", equalTo(true));
+    }
+
+    @Test
+    @DisplayName("Удаление курьера (запрос без id)")
+    public void deleteCourierWithUnsetIdThrowsError() {
+        // Arrange
+
+        // Act
+        Response response = CourierActions.delete("");
+
+        // Assert
+        response.then().assertThat().statusCode(HttpStatus.SC_BAD_REQUEST)
+                .and()
+                .body("message", equalTo("Недостаточно данных для удаления курьера"));
+    }
+
+    @Test
+    @DisplayName("Удаление курьера с неверным id")
+    public void deleteCourierWithIncorrectIdThrowsError() {
+        // Arrange
+
+        // Act
+        Response response = CourierActions.delete("222");
+
+        // Assert
+        response.then().assertThat().statusCode(HttpStatus.SC_NOT_FOUND)
+                .and()
+                .body("message", equalTo("Курьера с таким id нет."));
+    }
+}
